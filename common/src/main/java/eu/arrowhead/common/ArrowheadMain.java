e9b7894fa6d58a1d376b89fc875e82bcc043cccb
Mario Zsilak <17300435+mzsilak@users.noreply.github.com>
2018-09-09 22:14:02 +0200

    added swagger

--- common/src/main/java/eu/arrowhead/common/ArrowheadMain.java
+++ common/src/main/java/eu/arrowhead/common/ArrowheadMain.java
@@ -71,7 +71,7 @@ public abstract class ArrowheadMain {
   protected void init(CoreSystem coreSystem, String[] args, Set<Class<?>> classes, String[] packages) {
     System.out.println("Working directory: " + System.getProperty("user.dir"));
     this.coreSystem = coreSystem;
-    DatabaseManager.init();
+//    DatabaseManager.init();
 
     boolean isSecure = false;
     for (String arg : args) {
@@ -143,10 +143,12 @@ public abstract class ArrowheadMain {
     final ResourceConfig config = new ResourceConfig();
     config.registerClasses(classes);
     config.packages(packages);
+    adaptConfig(config);
 
     URI uri = UriBuilder.fromUri(baseUri).build();
     try {
       server = GrizzlyHttpServerFactory.createHttpServer(uri, config, false);
+      adaptServer(server);
       server.getServerConfiguration().setAllowPayloadForUndefinedHttpMethods(true);
       server.start();
       log.info("Started server at: " + baseUri);
@@ -157,10 +159,19 @@ public abstract class ArrowheadMain {
     }
   }
 
-  protected void startSecureServer(Set<Class<?>> classes, String[] packages) {
+  protected void adaptServer(HttpServer server) {
+		// NOOP
+	}
+
+	protected void adaptConfig(ResourceConfig config) {
+		// NOOP
+	}
+
+	protected void startSecureServer(Set<Class<?>> classes, String[] packages) {
     final ResourceConfig config = new ResourceConfig();
     config.registerClasses(classes);
     config.packages(packages);
+    adaptConfig(config);
 
     String keystorePath = props.getProperty("keystore");
     String keystorePass = props.getProperty("keystorepass");
--- systemregistry/src/main/java/eu/arrowhead/core/systemregistry/SystemRegistryMain.java
+++ systemregistry/src/main/java/eu/arrowhead/core/systemregistry/SystemRegistryMain.java
@@ -13,23 +13,37 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.glassfish.grizzly.http.server.CLStaticHttpHandler;
+import org.glassfish.grizzly.http.server.HttpHandler;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.server.ResourceConfig;
+
 import eu.arrowhead.common.ArrowheadMain;
 import eu.arrowhead.common.misc.CoreSystem;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 
 public class SystemRegistryMain extends ArrowheadMain {
 	private SystemRegistryMain(String[] args) {
-		Set<Class<?>> classes = new HashSet<>(Arrays.asList(SystemRegistryResource.class, SystemRegistryApi.class));
+		Set<Class<?>> classes = new HashSet<>(Arrays.asList(SystemRegistryResource.class, SystemRegistryApi.class, OpenApiResource.class));
 		String[] packages = {
 				"eu.arrowhead.common.exception", 
 				"eu.arrowhead.common.json", 
 				"eu.arrowhead.common.filter", 
-				"eu.arrowhead.systemregistry", 
-				"eu.arrowhead.systemregistry.model", 
-				"eu.arrowhead.systemregistry.filter",
-				"io.swagger.jaxrs2.integration.resources"};
+				"eu.arrowhead.core.systemregistry", 
+				"eu.arrowhead.core.systemregistry.model", 
+				"eu.arrowhead.core.systemregistry.filter",
+				"io.swagger.v3.jaxrs2.integration.resources"};
 		init(CoreSystem.SYSTEMREGISTRY, args, classes, packages);
 		
 		listenForInput();
+	}
+	protected void adaptServer(HttpServer server) {
+		final HttpHandler httpHandler = new CLStaticHttpHandler(HttpServer.class.getClassLoader(), "/swagger/");
+		server.getServerConfiguration().addHttpHandler(httpHandler, "/swagger");
+	}
+	
+	@Override
+	protected void adaptConfig(ResourceConfig config) {
 	}
 
 	public static void main(String[] args) {
