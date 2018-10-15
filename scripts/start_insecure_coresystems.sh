86a3ebc4089830ce8b775cb534565ef00faf7663
Umlauf Zoltán <zoltan.umlauf@gmail.com>
2018-10-15 21:44:41 +0200

    removed sleep times from startup scripts (deprecated because of async SR query)

--- scripts/start_insecure_coresystems.sh
+++ scripts/start_insecure_coresystems.sh
@@ -1,40 +1,34 @@
 #!/bin/bash
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
 
 time_to_sleep=10s
 
-cd "$parent_path"
-
-echo Starting Core Systems - wait 1 minute
-#More sleep time between core systems might be needed on slower devices like a Raspberry Pi
+echo Starting Core Systems... Service initializations usually need ~ 20 seconds.
 
 cd ../serviceregistry_sql/target
 nohup java -jar arrowhead-serviceregistry-sql-4.0.jar -d -daemon &> insecure_sr.log &
 echo Service Registry started
-sleep ${time_to_sleep}
+sleep ${time_to_sleep} #wait for the Service Registry to fully finish loading up
 
 cd ../../authorization/target
 nohup java -jar arrowhead-authorization-4.0.jar -d -daemon &> insecure_auth.log &
 echo Authorization started
-sleep 10s
 
 cd ../../gateway/target
 nohup java -jar arrowhead-gateway-4.0.jar -d -daemon &> insecure_gateway.log &
 echo Gateway started
-sleep ${time_to_sleep}
 
 cd ../../eventhandler/target
 nohup java -jar arrowhead-eventhandler-4.0.jar -d -daemon &> insecure_eventhandler.log &
 echo Event Handler started
-sleep ${time_to_sleep}
 
 cd ../../gatekeeper/target
 nohup java -jar arrowhead-gatekeeper-4.0.jar -d -daemon &> insecure_gk.log &
 echo Gatekeeper started
-sleep ${time_to_sleep}
 
 cd ../../orchestrator/target
 nohup java -jar arrowhead-orchestrator-4.0.jar -d -daemon &> insecure_orch.log &
 echo Orchestrator started
-sleep ${time_to_sleep}
+
--- scripts/start_secure_coresystems.sh
+++ scripts/start_secure_coresystems.sh
@@ -1,40 +1,33 @@
 #!/bin/bash
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
 cd "$parent_path"
 
 time_to_sleep=10s
 
-echo Starting Core Systems - wait 1 minute
-#More sleep time between core systems might be needed on slower devices like a Raspberry Pi
+echo Starting Core Systems... Service initializations usually need ~ 20 seconds.
 
 cd ../serviceregistry_sql/target
 nohup java -jar arrowhead-serviceregistry-sql-4.0.jar -d -daemon -tls &> secure_sr.log &
 echo Service Registry started
-sleep ${time_to_sleep}
+sleep ${time_to_sleep} #wait for the Service Registry to fully finish loading up
 
 cd ../../authorization/target
 nohup java -jar arrowhead-authorization-4.0.jar -d -daemon -tls &> secure_auth.log &
 echo Authorization started
-sleep ${time_to_sleep}
 
 cd ../../gateway/target
 nohup java -jar arrowhead-gateway-4.0.jar -d -daemon -tls &> secure_gateway.log &
 echo Gateway started
-sleep ${time_to_sleep}
 
 cd ../../eventhandler/target
 nohup java -jar arrowhead-eventhandler-4.0.jar -d -daemon -tls &> secure_eventhandler.log &
 echo Event Handler started
-sleep ${time_to_sleep}
 
 cd ../../gatekeeper/target
 nohup java -jar arrowhead-gatekeeper-4.0.jar -d -daemon -tls &> secure_gk.log &
 echo Gatekeeper started
-sleep ${time_to_sleep}
 
 cd ../../orchestrator/target
 nohup java -jar arrowhead-orchestrator-4.0.jar -d -daemon -tls &> secure_orch.log &
 echo Orchestrator started
-sleep ${time_to_sleep}
